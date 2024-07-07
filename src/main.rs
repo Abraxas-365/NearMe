def4d85046b8853124f591a4f18dc75741c70bd6
Abraxas-365 <luisfmiranda8@gmail.com>
2024-07-07 14:25:47 -0500

    feat: added database functionality and configurations
    The commit includes adding new files for database configurations and utilities with PostgreSQL as the chosen database. It also includes establishing a new connection to PostgreSQL in the main.rs file. New modules for "utils" and "config" have also been added. The config module fetches the database URL from the environment variable. The database utility includes scripts for running migrations on the database.

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,22 @@
 #![allow(dead_code)]
 
+use std::sync::Arc;
+
 use actix_cors::Cors;
 use actix_web::{middleware::Logger, App, HttpServer};
 
+use crate::{modules::product, utils::database::PostgresRepository};
+
 mod error;
 mod modules;
+mod utils;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     std::env::set_var("RUST_LOG", "info,debug");
     env_logger::init();
+
+    let repo = Arc::new(PostgresRepository::new().await);
 
     log::info!("Starting HTTP server on 0.0.0.0:80...");
     HttpServer::new(move || {
